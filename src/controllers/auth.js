9d2df4119e7d42e457209189b8de091d90b7e6e1
Connor <connorlwhite@icloud.com>
2024-08-14 14:45:57 -0400

    Added controllerand user.js

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,22 +1,33 @@
-import { signUp, getUser } from "../models/auth.js";
-import { validateAuthData } from "../schemas/auth.js";
-import { loginService } from "../services/auth.js";
+import { signUp } from "../models/auth.js";
+import {
+  validateAuthData,
+  validateForgetPasswordData,
+  validateResetPasswordSchema,
+} from "../schemas/auth.js";
+import { invalidateToken } from "../models/tokens.js";
+import {
+  loginService,
+  handleForgotPassword,
+  changePassword,
+  resetPassword,
+} from "../services/auth.js";
+import { refreshTokens, refreshAccessToken } from "../services/tokens.js";
 import { hashPassword } from "../helpers/auth.js";
 import { ACCESS_TOKEN_NAME, REFRESH_TOKEN_NAME } from "../secrets.js";
+import {
+  ProgrammingError,
+  ValidationError,
+  AuthError,
+} from "../utils/exceptions.js";
 
-async function signUpController(req, res) {
+async function signUpController(req, res, next) {
   try {
     const { email, password } = req.body;
 
     const valid = validateAuthData(req.body);
 
     if (!valid) {
-      // If validation fails, return a 400 Bad Request with the validation errors
-      return res.status(400).json({
-        success: false,
-        message: "Invalid input data",
-        errors: validateAuthData.errors, // Ajv provides a list of errors
-      });
+      return next(new ValidationError("Password or Email invalid"));
     }
 
     const hashedPassword = await hashPassword(password);
@@ -27,65 +38,27 @@ async function signUpController(req, res) {
       message: "Created user",
     });
   } catch (error) {
-    console.log("Error with signup controller ", error);
-    return res.status(500).json({
-      success: false,
-      message: "Server error with signing up" + error,
-    });
+    console.log(error);
+    return next(new ProgrammingError());
   }
 }
 
-async function getUserController(req, res) {
-  try {
-    const { user_id } = req.params;
-    //Check if user logged in. Middleware or helper function
-    if (!user_id) {
-      return res.status(404).json({
-        success: false,
-        message: "No valid user_id",
-      });
-    }
-
-    const get_user = await getUser(user_id);
-
-    if (!get_user) {
-      return res.status(404).json({
-        success: false,
-        message: "Cannot find user with that id",
-      });
-    }
-
-    return res.status(200).json({
-      success: true,
-      message: "Found user with that id",
-      get_user,
-    });
-  } catch (error) {
-    console.log("Error with user controller ", error);
-    return res.status(500).json({
-      success: false,
-      message: "Server error with getting user " + error,
-    });
-  }
-}
-
-async function loginController(req, res) {
+async function loginController(req, res, next) {
   try {
     const { email, password } = req.body;
 
     const valid = validateAuthData(req.body);
 
     if (!valid) {
-      // If validation fails, return a 400 Bad Request with the validation errors
-      return res.status(400).json({
-        success: false,
-        message: "Invalid input data",
-        errors: validateAuthData.errors, // Ajv provides a list of errors
-      });
+      return next(new ValidationError("Password or Email invalid"));
     }
 
     const tokens = await loginService(email, password);
-    console.log(tokens)
+
+    if (!tokens) {
+      return next(new AuthError("Password or Email invalid"));
+    }
+    console.log(tokens);
     res.cookie(ACCESS_TOKEN_NAME, tokens.access_token, {
       httpOnly: true,
       secure: true,
@@ -103,8 +76,182 @@ async function loginController(req, res) {
       message: "User login success",
     });
   } catch (error) {
-    return;
+    console.log(error);
+    return next(new ProgrammingError());
   }
 }
 
-export { signUpController, getUserController, loginController };
+//Refresh the access and refresh
+async function refreshTokensController(req, res, next) {
+  try {
+    console.log(req.cookies);
+    const { refreshToken } = req.cookies;
+
+    if (!refreshToken) {
+      return next(new ValidationError("Refresh token is required"));
+    }
+
+    // Use the service to handle token refreshing
+    const { newAccessToken, newRefreshToken } = await refreshTokens(
+      refreshToken
+    );
+
+    res.cookie(ACCESS_TOKEN_NAME, newAccessToken, {
+      httpOnly: true,
+      secure: true,
+      path: "/",
+    });
+
+    res.cookie(REFRESH_TOKEN_NAME, newRefreshToken, {
+      httpOnly: true,
+      secure: true,
+      path: "/",
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Tokens refreshed successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+// Refresh the access token only
+async function refreshAccessTokenController(req, res, next) {
+  try {
+    const { refreshToken } = req.cookies;
+
+    if (!refreshToken) {
+      return next(new ValidationError("Refresh token is required"));
+    }
+
+    // Use the service to refresh only the access token
+    const { newAccessToken } = await refreshAccessToken(refreshToken);
+
+    res.cookie(ACCESS_TOKEN_NAME, newAccessToken, {
+      httpOnly: true,
+      secure: true,
+      path: "/",
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Access token refreshed successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function logoutController(req, res, next) {
+  try {
+    const { refreshToken } = req.cookies;
+
+    if (!refreshToken) {
+      return next(new ValidationError("Refresh token is required"));
+    }
+
+    // Invalidate the refresh token
+    await invalidateToken(refreshToken, "refresh");
+
+    // Clear the cookies
+    res.clearCookie(ACCESS_TOKEN_NAME, {
+      httpOnly: true,
+      secure: true,
+      path: "/",
+    });
+    res.clearCookie(REFRESH_TOKEN_NAME, {
+      httpOnly: true,
+      secure: true,
+      path: "/",
+    });
+
+    res.status(200).json({
+      success: true,
+      message: "Logged out successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+}
+
+async function forgotPasswordController(req, res, next) {
+  try {
+    const { emailOrPhone } = req.body;
+
+    const valid = validateForgetPasswordData(req.body);
+
+    if (!valid) {
+      return next(new ValidationError("Email or Phone number is required"));
+    }
+
+    // Handle the forgot password logic
+    await handleForgotPassword(emailOrPhone);
+
+    return res.status(200).json({
+      success: true,
+      message: "Password reset link/OTP has been sent to your email/phone",
+    });
+  } catch (error) {
+    console.log(error);
+    next(new ProgrammingError("Failed to process forgot password request"));
+  }
+}
+
+async function resetPasswordController(req, res, next) {
+  try {
+    const { token, newPassword } = req.body;
+
+    const valid = validateResetPasswordSchema(req.body);
+
+    if (!valid) {
+      return next(new ValidationError("Email or Phone number is required"));
+    }
+
+    const response = await resetPassword(token, newPassword);
+
+    res.status(200).json({
+      success: true,
+      message: "Password reset successfully",
+      response,
+    });
+  } catch (error) {
+    console.log(error);
+    next(new ProgrammingError("Failed to process reset password request"));
+  }
+}
+
+async function changePasswordController(req, res, next) {
+  try {
+    const { email, password } = req.body;
+
+    // const valid = validateAuthData(req.body);
+
+    // if (!valid) {
+    //   return next(new ValidationError("Password or Email invalid"));
+    // }
+
+    const response = await changePassword(email, password);
+
+    res.status(200).json({
+      success: true,
+      message: "Password reset successfully",
+      response,
+    });
+  } catch (error) {
+    console.log(error);
+    next(new ProgrammingError("Failed to process reset password request"));
+  }
+}
+
+export {
+  signUpController,
+  loginController,
+  refreshTokensController,
+  refreshAccessTokenController,
+  logoutController,
+  forgotPasswordController,
+  resetPasswordController,
+  changePasswordController,
+};
